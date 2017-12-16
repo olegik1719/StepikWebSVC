8dfafb9362ecc358d8aec8ddf2a81d9b49a46ada
olegik1719 <oleg.mihaylov@gmail.com>
2017-12-16 22:42:58 +0300

    Add Templates for SignIn and SignUp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,22 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import javax.servlet.http.HttpServlet;
+
 public class Main {
     public static void main(String[] args) throws Exception{
         Frontend frontend = new Frontend();
         Mirror mirror = new Mirror();
+        SignIn signIn = new SignIn();
+        SignUp signUp = new SignUp();
 
         Server server = new Server (8080);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         server.setHandler(context);
         context.addServlet(new ServletHolder(frontend), "/authform");
         context.addServlet(new ServletHolder(mirror), "/mirror");
+        context.addServlet(new ServletHolder(signIn), "/signin");
+        context.addServlet(new ServletHolder(signUp), "/signup");
         server.start();
         java.util.logging.Logger.getGlobal().info("Server started");
         server.join();
---
+++ src/main/java/SignIn.java
@@ -0,0 +1,20 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignIn extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+
+    }
+
+}
+
